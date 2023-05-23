280dbcdd19644c2edc5686f2d0fba8e3cdb10513
sdi2000200 <tsitsoliaggelos@gmail.com>
2023-05-24 01:42:15 +0300

    telos smr

--- create_input.sh
+++ create_input.sh
@@ -3,6 +3,12 @@
 input_file=$1
 num_lines=$2
 
+if [ $# -ne 2 ] 
+    then echo "Not enough command line arguments."
+         exit 
+fi
+
+
 if [ ! -e $input_file ] 
     then echo "The file Doesn't Exist"
          exit 1
@@ -41,11 +47,15 @@ for (( iterator=1; iterator<=LIMIT; iterator++ )); do
     let "number %= PARTIES  "
     let "number_of_characters =$((RANDOM % 10 + 3))"
     for ((iterator2=1; iterator2<=$number_of_characters; iterator2++)); do
-        number2=${characters:RANDOM % 26:1}
+        ran_num=$RANDOM
+        let "ran_num %=26 "
+        number2=${characters:ran_num:1}
         random_string+=$number2
     done
     for ((iterator3=1; iterator3<=$number_of_characters; iterator3++)); do
-        number3=${characters:RANDOM % 26:1}
+        ran_num2=$RANDOM
+        let "ran_num2 %=26 "
+        number3=${characters:ran_num2:1}
         #number3=${characters:RANDOM % ${#characters}:1}
         random_string2+=$number3
     done
