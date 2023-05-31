ca78fc302eb923b2bcc7a1dbdce8e54a4d97b7dc
sdi2000200 <tsitsoliaggelos@gmail.com>
2023-05-31 19:42:59 +0300

    poller.c

--- poller.c
+++ poller.c
@@ -18,7 +18,7 @@ struct sockaddr_in  address_of_server ;
 address_of_server.sin_family=AF_INET;
 address_of_server.sin_addr.s_addr=htonl(INADDR_ANY);
 address_of_server.sin_port=htons(port);
-return bind (socket,(struct sockaddr*)&address_of_server,sizeof(address_of_server)) ;
+return bind (the_socket,(struct sockaddr*)&address_of_server,sizeof(address_of_server)) ;
 }
 
 int listening(int the_socket , int waiting_number){
@@ -29,13 +29,72 @@ int accepting(int the_socket ){
 struct sockaddr_in address_of_client;
 socklen_t * length;
 //struct sockaddr * pointer_to_address =( struct sockaddr *) & address_of_client ;
-return accept ( the_socket , (struct sockaddr*)&address_of_client ,&length);
+return accept ( the_socket , (struct sockaddr*)&address_of_client ,length);
+}
+
+
+
+int main(/*int argc , char * argv []*/){
+
+int portnum=5634;//atoi(argv[1]);
+int numWorkerthreads=8;//atoi(argv[2]);
+int bufferSize=16;//atoi(argv[3]);
+char *poll_log="";//(argv [4]);
+char* poll_stats="";//(argv [5]);
+
+
+int socket_fd=creating_socket();
+if (socket_fd<0){
+    printf("Error creating socket\n");
+    return -1;
+}
+
+
+int binding_error= binding(socket_fd,portnum);
+if (binding_error<0){
+    printf("Error in binding socket\n");
+    return -1;
+}
+
+
+int listening_error=listening(socket_fd,16);
+if (listening_error<0){
+    printf("Error in listening \n");
+    return -1;
+}
+
+
+
+while(1){
+
+int connect=accepting(socket_fd);
+if (connect<0){
+    printf("Error in accepting \n");
+    return -1;
 }
 
 
 
 
 
-int main(){
+}
+
+
+
+for ( i =0 ; i < n ; i ++) {
+sl = random () % MAX_SLEEP + 1; /* Sleeping time 1.. MAX_SLEEP */
+if ( err = pthread_create ( tids +i , NULL , sleeping , ( void *) sl ) ) {
+/* Create a thread */
+perror2 ( " pthread_create " , err ) ;
+exit (1) ;
+}
+}
+
+
+
+
+
+
+
 return 0;
 }
