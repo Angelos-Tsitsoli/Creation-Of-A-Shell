9a629c83d0e03299d43e24b8b4d94dedcabe1db1
sdi2000200 <tsitsoliaggelos@gmail.com>
2023-06-01 22:52:56 +0300

    poller.c

--- poller.c
+++ poller.c
@@ -6,6 +6,34 @@
 #include <arpa/inet.h> 
 #include <sys/socket.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+
+pthread_mutex_t mut ;
+pthread_cond_t con_v ;
+
+
+void * sleeping_untill_siganl(){
+    pthread_mutex_lock(&mut);
+    pthread_cond_wait(&con_v,&mut);
+    pthread_mutex_unlock(&mut);
+    pthread_exit (NULL);
+}
+
+
+void * signal(){
+    pthread_mutex_lock (&mut);
+    pthread_cond_signal (&con_v);
+    printf("SEND NAME PLEASE\n");
+    read ( newsock , buf , 1)
+    pthread_mutex_unlock(&mut);
+    pthread_exit ( NULL ) ;
+}
+
+
+
 
 int creating_socket(void){ //Function to create the socket.
 return socket (AF_INET,SOCK_STREAM,0);//AF_INET as communication domain , SOCK_STREAM as communication semantics and 0 as the one protocol that can be used.
@@ -29,10 +57,10 @@ int accepting(int the_socket ){
 struct sockaddr_in address_of_client;
 socklen_t * length;
 //struct sockaddr * pointer_to_address =( struct sockaddr *) & address_of_client ;
-return accept ( the_socket , (struct sockaddr*)&address_of_client ,length);
+return accept ( the_socket , NULL ,NULL); ///////////////////////////SHMANTIKO THELEI NULL TO DEYTERO
 }
 
-
+static int buffer_iterator=0;
 
 int main(/*int argc , char * argv []*/){
 
@@ -41,6 +69,24 @@ int numWorkerthreads=8;//atoi(argv[2]);
 int bufferSize=16;//atoi(argv[3]);
 char *poll_log="";//(argv [4]);
 char* poll_stats="";//(argv [5]);
+
+int * storing_buffer=malloc(bufferSize * sizeof(int));
+
+pthread_mutex_init(&mut,NULL); 
+pthread_cond_init(&con_v,NULL);
+
+
+
+///////////////////////[     1      ]////////////////////////////////////////////
+pthread_t * Thread; //Process of creating the threads
+Thread= malloc( numWorkerthreads * sizeof ( pthread_t ) );
+for ( int i =0 ; i < numWorkerthreads ; i ++) {//Creating threads .
+ pthread_create( Thread + i , NULL , sleeping_untill_siganl , NULL ); //THELEI FTIAKSIMOOOO 
+
+}
+///////////////////////////////////////////////////////////////////
+
+
 
 
 int socket_fd=creating_socket();
@@ -65,25 +111,20 @@ if (listening_error<0){
 
 
 
+
 while(1){
 
+///////////////////////[     2 kai 3      ]////////////////////////////////////////////
 int connect=accepting(socket_fd);
 if (connect<0){
     printf("Error in accepting \n");
     return -1;
 }
 
+storing_buffer[buffer_iterator]=connect;
+buffer_iterator++;
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-
-
-
-}
-
-
-pthread_t * Thread; //Process of creating the threads
-Thread= malloc( 8 * sizeof ( pthread_t ) );
-for ( int i =0 ; i < 8 ; i ++) {//Creating threads .
- pthread_create( Thread +i , NULL , NULL , NULL ); //THELEI FTIAKSIMOOOO 
 
 }
 
@@ -92,6 +133,10 @@ for ( int i =0 ; i < 8 ; i ++) {//Creating threads .
 
 
 
+
+
+
+free(storing_buffer);
 
 return 0;
 }
